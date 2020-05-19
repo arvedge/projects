01cba8a699a71132a1cc40502d30245cc017bfa8
arvedge <65608782+arvedge@users.noreply.github.com>
2020-05-19 16:10:36 +0200

    Add files via upload

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test
+{
+    public static void main(String[] args)
+    {
+        Graf5wierzcholkow G=new Graf5wierzcholkow();
+        G.wypisz_macierz();
+        G.wczytaj_macierz();
+        G.wypisz_macierz();
+    }
+}
